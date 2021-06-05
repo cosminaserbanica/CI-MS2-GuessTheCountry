ba28850bf3180f908d5bf20edce42b3760d0cdd8
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-06-05 12:58:13 +0100

    Added function for instructions modal

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,21 @@ const modal1 = document.getElementById('modal1');
 const closeModal1 = document.getElementById('close-modal1');
 
 openPlay.addEventListener('click', () => {
-  modal1.classList.add('show-modal1')
+  modal1.classList.add('show-modal')
 });
 
 closeModal1.addEventListener('click', () => {
-  modal1.classList.remove('show-modal1')
+  modal1.classList.remove('show-modal')
+});
+
+const openInstructions = document.getElementById('open-instructions');
+const modal2 = document.getElementById('modal2');
+const closeModal2 = document.getElementById('close-modal2');
+
+openInstructions.addEventListener('click', () => {
+  modal2.classList.add('show-modal')
+});
+
+closeModal2.addEventListener('click', () => {
+  modal2.classList.remove('show-modal')
 });
