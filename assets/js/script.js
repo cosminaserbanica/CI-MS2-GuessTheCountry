f857254aa6d68b59123f697955ea2dc215da0f9e
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-06-17 09:31:41 +0100

    Add function to close modal by clicking outside it

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ closeModal1.addEventListener('click', () => {
   modal1.classList.remove('show-modal')
 });
 
-
 const openInstructions = document.getElementById('open-instructions');
 const modal2 = document.getElementById('modal2');
 const closeModal2 = document.getElementById('close-modal2');
@@ -30,6 +29,18 @@ const closeModal3 = document.getElementById('close-modal3');
 openContact.addEventListener('click', () => {
   modal3.classList.add('show-modal')
 });
+
 closeModal3.addEventListener('click', () => {
   modal3.classList.remove('show-modal')
 });
+
+// Close modal by clicking anywhere outside of the modal area
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.classList.remove('show-modal')
+  } else if (event.target == modal2) {
+    modal2.classList.remove('show-modal')
+  } else if (event.target == modal3) {
+    modal3.classList.remove('show-modal')
+  } 
+}
