4dcb79418a27bcbcfa7b4dd9e187506fff2e828a
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-06-25 21:36:14 +0100

    Modal for capital cities option removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-const openPlay = document.getElementById('open-play');
-const modal1 = document.getElementById('modal1');
-const closeModal1 = document.getElementById('close-modal1');
-
-openPlay.addEventListener('click', () => {
-  modal1.classList.add('show-modal')
-});
-
-closeModal1.addEventListener('click', () => {
-  modal1.classList.remove('show-modal')
-});
-
 const openInstructions = document.getElementById('open-instructions');
 const modal2 = document.getElementById('modal2');
 const closeModal2 = document.getElementById('close-modal2');
@@ -36,9 +24,7 @@ closeModal3.addEventListener('click', () => {
 
 // Close modal by clicking anywhere outside of the modal area
 window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.classList.remove('show-modal')
-  } else if (event.target == modal2) {
+  if (event.target == modal2) {
     modal2.classList.remove('show-modal')
   } else if (event.target == modal3) {
     modal3.classList.remove('show-modal')
